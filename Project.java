2a0dd864c9eaa8fb1d4d3189e68d39a6aed89afe
BandlaMadhuri <118246512+BandlaMadhuri@users.noreply.github.com>
2025-07-08 14:28:31 +0530

    Add files via upload

---
+++ Project.java
@@ -0,0 +1,100 @@
+package training;
+import java.util.Scanner;
+public class Product {
+	public static final String Id = null;
+	// TODO Auto-generated method stub
+	public  float quantity = 0;
+	public int productId;
+	public String productname;
+	public String productcategory;
+	public String productquantity;
+	public double productsellingprice;
+	public double productbuyingprice;
+	public String addProduct;
+	public String searchProject;
+	public String ListByName;
+	public double searchByMoney;
+	public double checkTotalAmount;
+	public String profitByCategory;
+	
+	
+	
+	public float getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(float quantity) {
+		this.quantity = quantity;
+	}
+	public int getProductId() {
+		return productId;
+	}
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+	public String getProductname() {
+		return productname;
+	}
+	public void setProductname(int i) {
+		this.productname = i;
+	}
+	public String getProductcategory() {
+		return productcategory;
+	}
+	public void setProductcategory(String i) {
+		this.productcategory = i;
+	}
+	public String getProductquantity() {
+		return productquantity;
+	}
+	public void setProductquantity(int i) {
+		this.productquantity = i;
+	}
+	public double getProductsellingprice() {
+		return productsellingprice;
+	}
+	public void setProductsellingprice(double productsellingprice) {
+		this.productsellingprice = productsellingprice;
+	}
+	public double getProductbuyingprice() {
+		return productbuyingprice;
+	}
+	public void setProductbuyingprice(double productbuyingprice) {
+		this.productbuyingprice = productbuyingprice;
+	}
+	public String getAddProduct() {
+		return addProduct;
+	}
+	public void setAddProduct(String addProduct) {
+		this.addProduct = addProduct;
+	}
+	public String getSearchProject() {
+		return searchProject;
+	}
+	public void setSearchProject(String searchProject) {
+		this.searchProject = searchProject;
+	}
+	public String getListByName() {
+		return ListByName;
+	}
+	public void setListByName(String listByName) {
+		ListByName = listByName;
+	}
+	public double getSearchByMoney() {
+		return searchByMoney;
+	}
+	public void setSearchByMoney(double searchByMoney) {
+		this.searchByMoney = searchByMoney;
+	}
+	public double getCheckTotalAmount() {
+		return checkTotalAmount;
+	}
+	public void setCheckTotalAmount(double checkTotalAmount) {
+		this.checkTotalAmount = checkTotalAmount;
+	}
+	public String getProfitByCategory() {
+		return profitByCategory;
+	}
+	public void setProfitByCategory(String profitByCategory) {
+		this.profitByCategory = profitByCategory;
+	}
+	}
